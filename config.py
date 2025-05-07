bbc82b4a9ed6679199092f619312fba6ea42f088
YunanLi <71833505+YunanL@users.noreply.github.com>
2025-05-07 10:10:13 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+    "INFURA_URL":"https://sepolia.infura.io/v3/835d10ec0e834928b10658f71faffe74",
+
+    "NFT_contract":"0x0db3bbc19549f724c7da68edbdf4048afc6a242d",
+
+    "Machine2": "0xe78785561Cc77dE5251dfA68027D826CAB1FcFa1",
+
+    "Machine2PK": "7823fa774285f1955a63e56feb9f5eeb18256f0a24bbc8dd230a7233124e490b",
+
+    "Receiver": "0x67467e51C1D78e02648f140FF1886bBbd4E2158c",
+
+}
